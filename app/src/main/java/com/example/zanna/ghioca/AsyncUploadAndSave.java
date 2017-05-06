8243f05819807c602254f00f83c399418d556a58
mzanella <zanna0150@gmail.com>
2017-05-06 12:58:56 +0200

    Changed camera library

--- app/src/main/java/com/example/zanna/ghioca/AsyncUploadAndSave.java
+++ app/src/main/java/com/example/zanna/ghioca/AsyncUploadAndSave.java
@@ -8,9 +8,6 @@ import android.os.AsyncTask;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.io.File;
-import java.io.FileInputStream;
-
 /**
  * @author Marco Zanella
  * @version 0.01
@@ -69,9 +66,12 @@ public class AsyncUploadAndSave extends AsyncTask<String, Void, Void> {
             @Override
             public void onSuccess(JSONObject answer) {
                 try {
-                    String s = answer.getString("best_guess");
-                    if (s == null)
-                        s = "No result found";
+                    String s = "No result found";
+                    if (answer != null) {
+                        String temp = answer.getString("best_guess");
+                        if (temp != null)
+                            s = temp;
+                    }
                     resultDialog.setMessage(s);
                     resultDialog.show();
                 } catch (JSONException e) {
@@ -84,13 +84,6 @@ public class AsyncUploadAndSave extends AsyncTask<String, Void, Void> {
     @Override
     protected Void doInBackground(String... params) {
         try {
-            File file = new File(params[0]);
-            byte[] jpeg = new byte[(int) file.length()];
-
-            FileInputStream fis = new FileInputStream(file);
-            fis.read(jpeg); //read file into bytes[]
-            fis.close();
-
             UploadingUtility.uploadToServer("file://" + params[0],MyApplication.getAppContext(), uploadingListener);
         } catch (Exception e) {
             e.printStackTrace();
--- app/src/main/java/com/example/zanna/ghioca/MainActivity.java
+++ app/src/main/java/com/example/zanna/ghioca/MainActivity.java
@@ -1,141 +1,275 @@
 package com.example.zanna.ghioca;
 
-import android.content.Intent;
-import android.net.Uri;
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.RequiresPermission;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.view.ViewCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import com.flurgle.camerakit.CameraKit;
-import com.flurgle.camerakit.CameraListener;
-import com.flurgle.camerakit.CameraView;
+import com.github.florent37.camerafragment.CameraFragment;
+import com.github.florent37.camerafragment.CameraFragmentApi;
+import com.github.florent37.camerafragment.configuration.Configuration;
+import com.github.florent37.camerafragment.listeners.CameraFragmentControlsAdapter;
+import com.github.florent37.camerafragment.listeners.CameraFragmentResultAdapter;
+import com.github.florent37.camerafragment.listeners.CameraFragmentStateAdapter;
+import com.github.florent37.camerafragment.widgets.CameraSettingsView;
+import com.github.florent37.camerafragment.widgets.CameraSwitchView;
+import com.github.florent37.camerafragment.widgets.FlashSwitchView;
+import com.github.florent37.camerafragment.widgets.RecordButton;
 
 import java.io.File;
 import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 import io.filepicker.Filepicker;
 
 import static com.example.zanna.ghioca.MyApplication.MY_API_KEY;
+import static org.bouncycastle.asn1.ua.DSTU4145NamedCurves.params;
 
 public class MainActivity extends AppCompatActivity {
-    final int SELECT_PICTURE = 100;
-    CameraView cameraView;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        cameraView.start();
-    }
+    public static final String FRAGMENT_TAG = "camera";
+    private static final int REQUEST_CAMERA_PERMISSIONS = 931;
+    private static final int REQUEST_PREVIEW_CODE = 1001;
+    @Bind(R.id.settings_view)
+    CameraSettingsView settingsView;
+    @Bind(R.id.flash_switch_view)
+    FlashSwitchView flashSwitchView;
+    @Bind(R.id.front_back_camera_switcher)
+    CameraSwitchView cameraSwitchView;
+    @Bind(R.id.record_button)
+    RecordButton recordButton;
 
-    @Override
-    protected void onPause() {
-        cameraView.stop();
-        super.onPause();
-    }
+    @Bind(R.id.record_duration_text)
+    TextView recordDurationText;
+    @Bind(R.id.record_size_mb_text)
+    TextView recordSizeText;
+
+    @Bind(R.id.cameraLayout)
+    View cameraLayout;
+    @Bind(R.id.addCameraButton)
+    View addCameraButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Filepicker.setKey(MY_API_KEY);
         setContentView(R.layout.activity_main);
-        SavingUtility.runTimePermissionAcquirement(this);
+        ButterKnife.bind(this);
 
-        Button imgPicker = (Button)findViewById(R.id.pick_file);
-        imgPicker.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-                intent.addCategory(Intent.CATEGORY_OPENABLE);
-                intent.setType("image/*");
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), SELECT_PICTURE);
-            }
-        });
+        // acquiring permission runtime
+        final String[] permissions = {
+                Manifest.permission.CAMERA,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                Manifest.permission.READ_EXTERNAL_STORAGE};
 
-        cameraView = (CameraView)findViewById(R.id.camera);
-        final ImageButton flash = (ImageButton)findViewById(R.id.btn_flash);
-        cameraView.setFlash(CameraKit.Constants.FLASH_OFF);
-
-        flash.setOnClickListener(new View.OnClickListener() {
-            boolean flashOn = true;
-
-            @Override
-            public void onClick(View v) {
-                flashOn = !flashOn;
-                if (flashOn){
-                    cameraView.setFlash(CameraKit.Constants.FLASH_ON);
-                    flash.setImageDrawable(getResources().getDrawable(R.drawable.ic_flash_on));
-                } else {
-                    cameraView.setFlash(CameraKit.Constants.FLASH_OFF);
-                    flash.setImageDrawable(getResources().getDrawable(R.drawable.ic_flash_off));
-                }
-            }
-        });
-
-        ImageButton camera = (ImageButton)findViewById(R.id.btn_camera);
-        camera.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                cameraView.captureImage();
-            }
-        });
-
-        cameraView.setCameraListener(new CameraListener() {
-            String name;
-
-            @Override
-            public void onPictureTaken(final byte[] jpeg) {
-                super.onPictureTaken(jpeg);
-                name = photoName();
-                new AsyncTask<Void, Void, Void>() {
-                    @Override
-                    protected Void doInBackground(Void... params) {
-                        SavingUtility.saveFile(jpeg, name, MainActivity.this);
-                        return null;
-                    }
-
-                    @Override
-                    protected void onPostExecute(Void aVoid) {
-                        new AsyncUploadAndSave()
-                                .setContextActivity(MainActivity.this)
-                                .execute(SavingUtility.folderPath + File.separator + name, null, null);
-                    }
-                }.execute(null, null, null);
-
-
-
-            }
-
-
-        });
-    }
-
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (resultCode == RESULT_OK) {
-            if (requestCode == SELECT_PICTURE) {
-                // Get the url from data
-                final Uri selectedImageUri = data.getData();
-                if (null != selectedImageUri) {
-                    new AsyncUploadAndSave()
-                            .setContextActivity(MainActivity.this)
-                            .execute(ConvertUriToFilePath.getPathFromURI(MainActivity.this,
-                                    selectedImageUri), null, null);
-                }
-
+        final List<String> permissionsToRequest = new ArrayList<>();
+        for (String permission : permissions) {
+            if (ActivityCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED) {
+                permissionsToRequest.add(permission);
             }
         }
+        if (!permissionsToRequest.isEmpty()) {
+            ActivityCompat.requestPermissions(this, permissionsToRequest.toArray(new String[permissionsToRequest.size()]), REQUEST_CAMERA_PERMISSIONS);
+        } else addCamera();
+        // TODO -> put in more appropriate place
+        Filepicker.setKey(MY_API_KEY);
+        //SavingUtility.runTimePermissionAcquirement(this);
+    }
+
+    @OnClick(R.id.flash_switch_view)
+    public void onFlashSwitchClicked() {
+        final CameraFragmentApi cameraFragment = getCameraFragment();
+        if (cameraFragment != null) {
+            cameraFragment.toggleFlashMode();
+        }
+    }
+
+    @OnClick(R.id.front_back_camera_switcher)
+    public void onSwitchCameraClicked() {
+        final CameraFragmentApi cameraFragment = getCameraFragment();
+        if (cameraFragment != null) {
+            cameraFragment.switchCameraTypeFrontBack();
+        }
+    }
+
+    @OnClick(R.id.record_button)
+    public void onRecordButtonClicked() {
+        final CameraFragmentApi cameraFragment = getCameraFragment();
+        if (cameraFragment != null) {
+            final String name = photoName();
+            cameraFragment.takePhotoOrCaptureVideo(new CameraFragmentResultAdapter() {
+                @Override
+                public void onPhotoTaken(final byte[] bytes, String filePath) {
+                    Toast.makeText(getBaseContext(), "onPhotoTaken " + filePath, Toast.LENGTH_SHORT).show();
+                    
+                    new AsyncTask<Void, Void, Void>(){
+                        @Override
+                        protected Void doInBackground(Void... params) {
+                            //SavingUtility.saveFile(bytes, name + ".jpeg", MainActivity.this);
+                            boolean b = false;
+                            File f;
+                            while (!b) {
+                                try {
+                                    Thread.sleep(1000);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                f = new File(SavingUtility.folderPath, name + ".jpg");
+                                b = f.exists();
+                            }
+
+                            return null;
+                        }
+
+                        @Override
+                        protected void onPostExecute(Void aVoid) {
+                            new AsyncUploadAndSave()
+                                    .setContextActivity(MainActivity.this)
+                                    .execute(SavingUtility.folderPath + File.separator + name + ".jpg", null, null);
+                        }
+                    }.execute(null, null, null);
+                }
+            }, SavingUtility.folderPath, name);
+        }
+    }
+
+    @OnClick(R.id.settings_view)
+    public void onSettingsClicked() {
+        final CameraFragmentApi cameraFragment = getCameraFragment();
+        if (cameraFragment != null) {
+            cameraFragment.openSettingDialog();
+        }
+    }
+
+    /*@OnClick(R.id.addCameraButton)
+    public void onAddCameraClicked() {
+
+    }*/
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (grantResults.length != 0) {
+            addCamera();
+        }
+    }
+
+    @RequiresPermission(Manifest.permission.CAMERA)
+    public void addCamera() {
+        addCameraButton.setVisibility(View.GONE);
+        cameraLayout.setVisibility(View.VISIBLE);
+
+        final CameraFragment cameraFragment = CameraFragment.newInstance(new Configuration.Builder()
+                .setCamera(Configuration.CAMERA_FACE_REAR).build());
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.content, cameraFragment, FRAGMENT_TAG)
+                .commitAllowingStateLoss();
+
+        if (cameraFragment != null) {
+            //cameraFragment.setResultListener(new CameraFragmentResultListener() {
+            //    @Override
+            //    public void onVideoRecorded(String filePath) {
+            //        Intent intent = PreviewActivity.newIntentVideo(MainActivity.this, filePath);
+            //        startActivityForResult(intent, REQUEST_PREVIEW_CODE);
+            //    }
+//
+            //    @Override
+            //    public void onPhotoTaken(byte[] bytes, String filePath) {
+            //        Intent intent = PreviewActivity.newIntentPhoto(MainActivity.this, filePath);
+            //        startActivityForResult(intent, REQUEST_PREVIEW_CODE);
+            //    }
+            //});
+
+            cameraFragment.setStateListener(new CameraFragmentStateAdapter() {
+
+                @Override
+                public void onCurrentCameraBack() {
+                    cameraSwitchView.displayBackCamera();
+                }
+
+                @Override
+                public void onCurrentCameraFront() {
+                    cameraSwitchView.displayFrontCamera();
+                }
+
+                @Override
+                public void onFlashAuto() {
+                    flashSwitchView.displayFlashAuto();
+                }
+
+                @Override
+                public void onFlashOn() {
+                    flashSwitchView.displayFlashOn();
+                }
+
+                @Override
+                public void onFlashOff() {
+                    flashSwitchView.displayFlashOff();
+                }
+
+                @Override
+                public void shouldRotateControls(int degrees) {
+                    ViewCompat.setRotation(cameraSwitchView, degrees);
+                    ViewCompat.setRotation(flashSwitchView, degrees);
+                    ViewCompat.setRotation(recordDurationText, degrees);
+                    ViewCompat.setRotation(recordSizeText, degrees);
+                }
+
+
+                @Override
+                public void onRecordStatePhoto() {
+                    recordButton.displayPhotoState();
+                }
+            });
+
+            cameraFragment.setControlsListener(new CameraFragmentControlsAdapter() {
+                @Override
+                public void lockControls() {
+                    cameraSwitchView.setEnabled(false);
+                    recordButton.setEnabled(false);
+                    settingsView.setEnabled(false);
+                    flashSwitchView.setEnabled(false);
+                }
+
+                @Override
+                public void unLockControls() {
+                    cameraSwitchView.setEnabled(true);
+                    recordButton.setEnabled(true);
+                    settingsView.setEnabled(true);
+                    flashSwitchView.setEnabled(true);
+                }
+
+                @Override
+                public void allowCameraSwitching(boolean allow) {
+                    cameraSwitchView.setVisibility(allow ? View.VISIBLE : View.GONE);
+                }
+
+                @Override
+                public void allowRecord(boolean allow) {
+                    recordButton.setEnabled(allow);
+                }
+            });
+
+        }
+    }
+
+    private CameraFragmentApi getCameraFragment() {
+        return (CameraFragmentApi) getSupportFragmentManager().findFragmentByTag(FRAGMENT_TAG);
     }
 
     private String photoName() {
         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
-        return timestamp.toString().replaceAll(" ", "_") + ".jpeg";
-    }
-
-    @Override
-    protected void onDestroy() {
-        Filepicker.unregistedLocalFileUploadCallbacks();
-        super.onDestroy();
+        return timestamp.toString().replaceAll(" ", "_");
     }
 }
