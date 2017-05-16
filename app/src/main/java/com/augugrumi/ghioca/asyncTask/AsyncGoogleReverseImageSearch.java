8fd871a1881852107711a18a8188f443b4451130
mzanella <zanna0150@gmail.com>
2017-05-16 10:52:58 +0200

    Implement async task for reverse google image search

--- app/src/main/java/com/augugrumi/ghioca/asyncTask/AsyncGoogleReverseImageSearch.java
+++ app/src/main/java/com/augugrumi/ghioca/asyncTask/AsyncGoogleReverseImageSearch.java
@@ -1,10 +1,67 @@
 package com.augugrumi.ghioca.asyncTask;
 
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.augugrumi.ghioca.listener.GoogleReverseImageSearchListener;
+
+import it.polpetta.libris.image.ReverseImageSearch;
+import it.polpetta.libris.image.google.contract.IGoogleImageSearchResult;
+
+import java.net.URL;
+
 /**
  * @author Marco Zanella
  * @version 0.01
  * @since 0.01
  */
 
-public class AsyncGoogleReverseImageSearch {
+public class AsyncGoogleReverseImageSearch extends AsyncTask<Void, Void, Void> {
+    private GoogleReverseImageSearchListener listener;
+    private IGoogleImageSearchResult result;
+    private boolean error;
+    private String url;
+    private Exception e;
+
+    public AsyncGoogleReverseImageSearch(String url, GoogleReverseImageSearchListener listener) {
+        this.listener = listener;
+        this.url = url;
+        error = false;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        super.onPreExecute();
+
+        listener.onStart();
+    }
+
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        try {
+            result = ReverseImageSearch
+                    .getGoogleServices()
+                    .imageSearchBuildQuery()
+                    .setImage(new URL(url))
+                    .build()
+                    .search();
+            Log.i("SEARCH_RESULT", result.toJSONString());
+        } catch (Exception error) {
+            e = error;
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Void aVoid) {
+        super.onPostExecute(aVoid);
+
+        // TODO think if it could be the right thing to do
+        if (error)
+            listener.onFailure(e);
+        else
+            listener.onSuccess(result);
+    }
 }
